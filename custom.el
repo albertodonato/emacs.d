31d33eca23b5bd4604bb70929fa9026ebf32773c
Alberto Donato <alberto.donato@gmail.com>
2015-10-12 17:29:21 +0300

    Update package list.

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (jedi-direx erlang yaml-mode go-mode full-ack multiple-cursors yasnippet rcirc-notify rcirc-color less-css-mode unicode-fonts web-mode smartparens scss-mode python-mode ox-reveal flycheck find-file-in-repository f ercn erc-view-log erc-hl-nicks autopair))))
+    (nginx-mode jedi-direx erlang yaml-mode go-mode full-ack multiple-cursors yasnippet rcirc-notify rcirc-color less-css-mode unicode-fonts web-mode smartparens scss-mode python-mode ox-reveal flycheck find-file-in-repository f ercn erc-view-log erc-hl-nicks autopair))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
