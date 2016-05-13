f2325a7765c7598c7b7f5df0201a1ed3edfc5efe
Alberto Donato <alberto.donato@gmail.com>
2016-05-13 17:28:14 +0300

    Add macro and more commands.

--- lisp/ack-irc-commands.el
+++ lisp/ack-irc-commands.el
@@ -8,9 +8,15 @@
 (require 'erc)
 (require 'subr-x)
 
-(defun erc-cmd-TABLEFLIP (&rest line)
-  "Flip table and send LINE."
-  (erc-send-message (concat "(╯°□°）╯︵ ┻━┻ " (string-join line " "))))
+(defmacro ack/def-erc-message-cmd (command description prefix)
+  "Define an ERC COMMAND with a DESCRIPTION and PREFIX text."
+  (let ((funcname (intern (concat "erc-cmd-" command))))
+    `(defun ,funcname (&rest line)
+     ,description
+     (erc-send-message (concat ,prefix (string-join line " "))))))
+
+(ack/def-erc-message-cmd "TABLEFLIP" "Flip table and send LINE." "(╯°□°）╯︵ ┻━┻ ")
+(ack/def-erc-message-cmd "TABLERESET" "Reset table and send LINE." "┬──┬◡ﾉ(° -°ﾉ)")
 
 (provide 'ack-irc-commands)
 ;;; ack-irc-commands.el ends here
