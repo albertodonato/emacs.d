6af3330b5c2e4fdaf34c4d923989a58ccf8a4acd
Alberto Donato <alberto.donato@gmail.com>
2017-05-20 16:36:41 +0200

    Add less-css-mode.

--- custom.el
+++ custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ample-theme restclient go-guru crontab-mode groovy-mode magit lua-mode flycheck ag projectile dockerfile-mode emamux markdown-mode jabber json erc-hl-nicks dired-subtree yasnippet yaml-mode emmet-mode expand-region smex flx-ido json-mode znc jinja2-mode nginx-mode jedi-direx erlang go-mode multiple-cursors unicode-fonts web-mode smartparens scss-mode ox-reveal find-file-in-repository f ercn erc-view-log autopair))))
+    (less-css-mode ample-theme restclient go-guru crontab-mode groovy-mode magit lua-mode flycheck ag projectile dockerfile-mode emamux markdown-mode jabber json erc-hl-nicks dired-subtree yasnippet yaml-mode emmet-mode expand-region smex flx-ido json-mode znc jinja2-mode nginx-mode jedi-direx erlang go-mode multiple-cursors unicode-fonts web-mode smartparens scss-mode ox-reveal find-file-in-repository f ercn erc-view-log autopair)))
+ '(unify-8859-on-decoding-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
