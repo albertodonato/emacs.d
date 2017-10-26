95ad10b7402b4be0e18412007aba5a9bca361388
Alberto Donato <alberto.donato@gmail.com>
2017-10-26 09:10:36 +0200

    update font

--- custom.el
+++ custom.el
@@ -13,5 +13,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :family "Ubuntu Mono" :height 102))))
- '(fixed-pitch ((t (:inherit nil :family "Ubuntu Mono" :height 102)))))
+ '(default ((t (:inherit nil :family "Ubuntu Mono" :height 90))))
+ '(fixed-pitch ((t (:inherit nil :family "Ubuntu Mono" :height 90)))))
