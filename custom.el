ad2d4f3f5d82c830170526a9a256e3238081efb9
Alberto Donato <alberto.donato@gmail.com>
2020-03-13 15:56:49 +0100

    fix font size

--- custom.el
+++ custom.el
@@ -4,13 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(emojify dpkg-dev-el lsp-ui lsp-mode jedi-core bash-completion py-isort protobuf-mode wsd-mode graphql-mode po-mode go-projectile less-css-mode ample-theme restclient go-guru crontab-mode magit lua-mode flycheck ag projectile dockerfile-mode emamux markdown-mode jabber json erc-hl-nicks dired-subtree yasnippet yaml-mode emmet-mode expand-region smex flx-ido json-mode znc jinja2-mode nginx-mode jedi-direx erlang go-mode multiple-cursors unicode-fonts web-mode smartparens scss-mode ox-reveal find-file-in-repository f ercn erc-view-log autopair))
- '(tramp-syntax 'default nil (tramp))
- '(unify-8859-on-decoding-mode t))
+   '(emojify dpkg-dev-el lsp-ui lsp-mode jedi-core bash-completion py-isort protobuf-mode wsd-mode graphql-mode po-mode go-projectile less-css-mode ample-theme restclient go-guru crontab-mode magit lua-mode flycheck ag projectile dockerfile-mode emamux markdown-mode jabber json erc-hl-nicks dired-subtree yasnippet yaml-mode emmet-mode expand-region smex flx-ido json-mode znc jinja2-mode nginx-mode jedi-di
+             rex erlang go-mode multiple-cursors unicode-fonts web-mode smartparens scss-mode ox-reveal find-file-in-repository f ercn erc-view-log autopair)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :family "Ubuntu Mono" :height 90))))
- '(fixed-pitch ((t (:inherit nil :family "Ubuntu Mono" :height 90)))))
+ '(default ((t (:inherit nil :family "Ubuntu Mono" :height 76))))
+ '(fixed-pitch ((t (:inherit nil :family "Ubuntu Mono" :height 76)))))
