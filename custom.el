c19425678ab01962ff14de41a91ec5b25ef8117f
Alberto Donato <alberto.donato@gmail.com>
2025-03-24 17:18:58 +0100

    add AI tools setup

--- custom.el
+++ custom.el
@@ -4,7 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(graphql-mode just-mode terraform-mode nerd-icons ample-theme bash-completion color-theme-sanityinc-tomorrow command-log-mode company company-lsp crm-custom crontab-mode deadgrep diff-hl dockerfile-mode doom-modeline dpkg-dev-el emojify erc-hl-nicks expand-region flx-ido flycheck git-commit go-mode go-projectile groovy-mode hcl-mode ido-completing-read+ jinja2-mode json-mode jsonnet-mode ledger-mode lsp-headerline lsp-mode lsp-pylsp lsp-pyright lsp-ui lua-mode lxd-tramp magit multiple-cursors nginx-mode nix-mode po-mode projectile restclient rust-mode scratch scss-mode smartparens smex use-package vterm web-mode webpaste wsd-mode yaml-mode yasnippet znc)))
+   '(aider ample-theme bash-completion color-theme-sanityinc-tomorrow
+           command-log-mode company company-lsp crm-custom crontab-mode
+           deadgrep diff-hl dockerfile-mode doom-modeline dpkg-dev-el emojify
+           erc-hl-nicks expand-region flx-ido flycheck git-commit go-mode
+           go-projectile gptel graphql-mode groovy-mode hcl-mode
+           ido-completing-read+ jinja2-mode json-mode jsonnet-mode just-mode
+           ledger-mode lsp-headerline lsp-mode lsp-pylsp lsp-pyright lsp-ui
+           lua-mode lxd-tramp magit multiple-cursors nerd-icons nginx-mode
+           nix-mode po-mode projectile restclient rust-mode scratch scss-mode
+           smartparens smex terraform-mode use-package vterm web-mode webpaste
+           wsd-mode yaml-mode yasnippet znc)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
